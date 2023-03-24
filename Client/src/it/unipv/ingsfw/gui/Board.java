346846725ab3d62391f4749f64872f60135df7f4
alessandrorotta <alessandrorotta@mbpdialessandro.homenet.telecomitalia.it>
2023-03-24 19:20:43 +0100

    minor fix

--- Client/src/it/unipv/ingsfw/gui/Board.java
+++ Client/src/it/unipv/ingsfw/gui/Board.java
@@ -17,18 +17,19 @@ public class Board extends JPanel {
 		tasti = new JToggleButton [8][8];
 		
 		setLayout(new GridLayout(8,8,0,0));
-		ImageIcon rookB = new ImageIcon("/Users/alessandrorotta/desktop/rookB.png");
+	/*	ImageIcon rookB = new ImageIcon("");
 		ImageIcon rookW = new ImageIcon("/Users/alessandrorotta/desktop/rookW.png");
 		ImageIcon queenB = new ImageIcon("/Users/alessandrorotta/desktop/queenB.png");
 		ImageIcon queenW = new ImageIcon("/Users/alessandrorotta/desktop/queenW.png");
 		ImageIcon bishopB = new ImageIcon("/Users/alessandrorotta/desktop/bishopB.png");
-		ImageIcon bishopW = new ImageIcon("/Users/alessandrorotta/desktop/bishopW.png");
-		ImageIcon kingB = new ImageIcon("/Users/alessandrorotta/desktop/kingB.png");
+		ImageIcon bishopW = new ImageIcon("/Users/alessandrorotta/desktop/bishopW.png"); */
+		ImageIcon kingB = new ImageIcon("/Progetto-P23/Client/img/kingB.png");
+	/*	
 		ImageIcon kingW = new ImageIcon("/Users/alessandrorotta/desktop/kingW.png");
 		ImageIcon pawnB = new ImageIcon("/Users/alessandrorotta/desktop/pawnB.png");
 		ImageIcon pawnW = new ImageIcon("/Users/alessandrorotta/desktop/pawnW.png");
 		ImageIcon knightB = new ImageIcon("/Users/alessandrorotta/desktop/knightB.png");
-		ImageIcon knightW = new ImageIcon("/Users/alessandrorotta/desktop/knightW.png");
+		ImageIcon knightW = new ImageIcon("/Users/alessandrorotta/desktop/knightW.png"); */
 		
 		
 		boolean white = true;
@@ -51,18 +52,19 @@ public class Board extends JPanel {
 				
 			}
 		}
-		tasti[1][1].setIcon(rookB);
+	/*	tasti[1][1].setIcon(rookB);
 		tasti[2][1].setIcon(rookW);
 		tasti[3][1].setIcon(queenB);
 		tasti[4][1].setIcon(queenW);
 		tasti[5][1].setIcon(bishopB);
-		tasti[6][1].setIcon(bishopW);
+		tasti[6][1].setIcon(bishopW); */
 		tasti[6][2].setIcon(kingB);
+	/*	
 		tasti[6][3].setIcon(kingW);
 		tasti[6][4].setIcon(pawnB);
 		tasti[6][5].setIcon(pawnW);
 		tasti[1][2].setIcon(knightB);
-		tasti[1][3].setIcon(knightW);
+		tasti[1][3].setIcon(knightW); */
 		
 		
 			
