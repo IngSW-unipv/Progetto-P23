5c6e5cff01903cf49bd1118dde4c277b288ce8c6
Fede <Lalo@192.168.1.2>
2023-05-21 18:23:28 +0200

    First implementation ServerSide

---
+++ Server/src/it/unipv/ingsfw/ClientHandler.java
@@ -0,0 +1,87 @@
+package it.unipv.ingsfw;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket clientSocket1;
+    private Socket clientSocket2;
+
+    // Constructor
+    public ClientHandler(Socket socket1, Socket socket2)
+    {
+        this.clientSocket1 = socket1;
+        this.clientSocket2 = socket2;
+    }
+
+    public void run()
+    {
+        PrintWriter out1 = null;
+        BufferedReader in1 = null;
+        PrintWriter out2 = null;
+        BufferedReader in2 = null;
+        try {
+                
+              // get the outputstream of client1
+            out1 = new PrintWriter(clientSocket1.getOutputStream(), true);
+
+              // get the inputstream of client1
+            in1 = new BufferedReader(new InputStreamReader(clientSocket1.getInputStream()));
+            
+              // get the outputstream of client2
+            out2 = new PrintWriter(clientSocket2.getOutputStream(), true);
+            
+            // get the inputstream of client2
+            in2 = new BufferedReader(new InputStreamReader(clientSocket2.getInputStream()));
+
+            String line1 = "WHITE";
+            String line2 = "BLACK";
+            
+            while (!line1.equals("Done") && !line2.equals("Done")) {
+				try {
+
+					line1 = in1.readLine();
+					System.out.println(line1);         
+					out2.println(line1);
+					
+					//if(line1.equals("Done")) break;
+					
+
+					line2 = in2.readLine();
+					System.out.println(line2);
+					out1.println(line2);
+					
+					//if(line2.equals("Done")) break;
+
+				} catch (IOException i) {
+					System.out.println(i);
+				}
+			}
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (out1 != null) {
+                    out1.close();
+                }
+                if (in1 != null) {
+                    in1.close();
+                    clientSocket1.close();
+                }
+                if (out2 != null) {
+                    out2.close();
+                }
+                if (in2 != null) {
+                    in2.close();
+                    clientSocket2.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
--- Server/src/it/unipv/ingsfw/Server.java
+++ Server/src/it/unipv/ingsfw/Server.java
@@ -1,6 +1,80 @@
 package it.unipv.ingsfw;
 
-public class Server {
-	
+import java.io.*;
+import java.net.*;
+  
+// Server class
+class Server {
+    public static void main(String[] args)
+    {
+        ServerSocket server = null;
+  
+        try {
+  
+            // server is listening on port 1234
+            server = new ServerSocket(1234);
+            server.setReuseAddress(true);
+  
+            // running infinite loop for getting
+            // client request
+            while (true) {
+  
+                // socket object to receive incoming client
+                // requests
+                Socket client1 = server.accept();
+  
+                // Displaying that new client is connected
+                // to server
+                System.out.println("New client1 connected"
+                                   + client1.getInetAddress()
+                                         .getHostAddress());
+                
+                String line = "White";
+                DataOutputStream output2 = new DataOutputStream(client1.getOutputStream());
+    			PrintStream os2 = new PrintStream(output2, false);
+    			//mando info ai client
+    			os2.println(line);
+               
+                Socket client2 = server.accept();
+                System.out.println("New client2 connected"
+                        + client2.getInetAddress()
+                              .getHostAddress());
+                DataInputStream in2 = new DataInputStream(new BufferedInputStream(client2.getInputStream()));
+    			//
 
+                DataOutputStream output = new DataOutputStream(client2.getOutputStream());
+    			PrintStream os = new PrintStream(output, false);
+    			
+
+    			String line2 = "Black";
+
+    			//mando info ai client
+    			os.println(line2);
+    			// inizio della partita
+    			os2.println("Inizia");
+  
+                // create a new thread object
+                ClientHandler clientSock
+                    = new ClientHandler(client1,client2);
+  
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (server != null) {
+                try {
+                    server.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
+  
